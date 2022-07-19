d5ffe33bd4708acb899a0d3e5014aa40eec3494d
Lucas Aguiar <lucas.abreu@redeceler.com.br>
2022-07-19 19:31:18 -0300

    O módulo de UI e o next-transpile-modules

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -1,12 +1,13 @@
 import { sum } from "@alura/utils/math/sum";
+import { Text } from "@alura/desing-system/components/Text";
 
 export default function HomeScreen() {
   return (
     <main>
-      <h1>home</h1>
-      <p>
+      <Text tag={"h1"}>Home</Text>
+      <Text tag="p">
         Importando módulo local: @alura/utils/math/sum sum(2, 2): {sum(2, 2)}
-      </p>
+      </Text>
     </main>
   );
 }
